f16d5c36401ec7180e9eec84ea3c8d26aaf29ba9
deglingo <ahmidmefes@gmail.com>
2014-10-05 00:59:26 +0200

    added simbot_init()

--- simbot/sbbase.c
+++ simbot/sbbase.c
@@ -1,2 +1,24 @@
 /* sbbase.c -
  */
+
+#include "simbot/sbbase.h"
+
+
+
+/* simbot_init_once:
+ */
+static gpointer simbot_init_once ( gpointer dummy )
+{
+  los_init();
+  return NULL;
+}
+
+
+
+/* simbot_init:
+ */
+void simbot_init ( void )
+{
+  GOnce once = G_ONCE_INIT;
+  g_once(&once, simbot_init_once, NULL);
+}
---
+++ simbot/sbbase.h
@@ -0,0 +1,15 @@
+/* sbbase.h -
+ */
+
+#ifndef _SBBASE_H_
+#define _SBBASE_H_
+
+#include <los.h>
+
+
+
+void simbot_init ( void );
+
+
+
+#endif
